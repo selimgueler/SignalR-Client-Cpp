fa4081a5e2691e224fe77141065f564c0fac5b96
Selim <selimgueler@gmail.com>
2020-04-23 17:43:23 +0200

    bugfix from previous merge

--- src/signalrclient/default_http_client.cpp
+++ src/signalrclient/default_http_client.cpp
@@ -41,7 +41,7 @@ namespace signalr
             {
                 headers.add(utility::conversions::to_string_t(header.first), utility::conversions::to_string_t(header.second));
             }
-			headers.add("Ebesinin ami", "Ebesinin aminin degeri");
+	
             http_request.headers() = headers;
         }
 
